ba5422d4f0148d66fa0eacec3f1330ff966b9e5b
Samsaribar <32449983+Samsaribar@users.noreply.github.com>
2017-10-31 08:47:12 +0100

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/samarafrey/Desktop/EPFL/BA Se 3/Programmation/SV Project/SV Project
+# Build directory: /Users/samarafrey/Desktop/EPFL/BA Se 3/Programmation/SV Project/SV Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
